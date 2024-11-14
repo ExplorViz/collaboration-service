357c4cc36c864f4cf9232f7bd9e131c67d0b5cc5
Malte Hansen <malte.hanseat@gmail.com>
2024-11-14 12:48:05 +0100

    Do not fix linting errors with npm run lint

--- src/message/client/sendable/user-disconnected-message.ts
+++ src/message/client/sendable/user-disconnected-message.ts
@@ -1,8 +1,8 @@
-import { HighlightingModel } from "src/model/highlighting-model";
+import { HighlightingModel } from 'src/model/highlighting-model';
 
 export const USER_DISCONNECTED_EVENT = 'user_disconnect';
 
 export type UserDisconnectedMessage = {
   id: string;
-  highlightedComponents: HighlightingModel[]
+  highlightedComponents: HighlightingModel[];
 };
